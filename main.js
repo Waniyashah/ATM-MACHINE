4c01993c5425c886178941c236d067a3f2ce4795
waniya shah <160254821+Waniyashah@users.noreply.github.com>
2024-04-01 23:10:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let mybalance = 1000;
+let mypincode = 567;
+let pinanswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin",
+        type: "number",
+    }
+]);
+if (pinanswer.pin === mypincode) {
+    console.log("Correct! Pin code");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Select the options",
+            type: "list",
+            choices: ["withdraw", "checkbalance"],
+        },
+    ]);
+    console.log(operationAns);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter your amount",
+                type: "number",
+            }
+        ]);
+        let withdrawalAmount = amountAns.amount;
+        if (withdrawalAmount <= mybalance) {
+            mybalance -= withdrawalAmount;
+            console.log("Withdrawal successful. Your remaining balance is: $" + mybalance);
+        }
+        else {
+            console.log("Insufficient balance. Your balance is: $" + mybalance);
+        }
+    }
+    else if (operationAns.operation === "checkbalance") {
+        console.log("Your balance is: $" + mybalance);
+    }
+}
+else {
+    console.log("Incorrect pin number");
+}
